f16385c4ee18e713f6f45c641bb8cf2954c8f31d
Leonardo-daVinci <nocturnus.labs4@gmail.com>
2022-08-15 19:06:09 -0700

    Homepage - Intro section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram, AiFillGithub} from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -10,15 +11,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className=' bg-blue-700 min-h-screen'>
-        <nav>
-          <h1>Leonardo-daVinci</h1>
-          <ul>
-              <li><BsFillMoonStarsFill/></li>
-              <li><a href='#'>Resume</a></li>
+      <main className=' bg-white px-10'>
+        <section className='min-h-screen'>
+        
+        {/* Navigation Section */}
+        
+        <nav className='py-10 mb-12 flex justify-between'>
+          <h1 className=' text-xl font-productsans'>Leonardo-daVinci</h1>
+          <ul className='flex items-center'>
+              <li><BsFillMoonStarsFill className=' cursor-pointer text-2xl'/></li>
+              <li><a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a></li>
           </ul>
         </nav>
+
+          {/* HomePage */}
+          
+          <div className=' text-center p-10 font-productsans'>
+            <h2 className=' text-5xl py-2 text-teal-600'>Akshit Abhay Keoliya</h2>
+            <h3 className=' text-2xl py-2'>Machine Learning Enthusiast and Developer</h3>
+            <p className=' text-md py-5 leading-8 text-gray-800'>Some sweet description for myself to highlight my profile</p>
+          </div>
+
+          <div className=' text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillLinkedin />
+            <AiFillGithub />
+            <AiFillTwitterCircle />
+            <AiFillInstagram />
+          </div>
+
         </section>
       </main>
     </>
