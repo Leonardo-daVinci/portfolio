a5b7f50854ccb77361aa4a2ac653bed21323be10
Leonardo-daVinci <37275791+Leonardo-daVinci@users.noreply.github.com>
2023-01-22 08:18:09 +0000

    Deploying to gh-pages from @ Leonardo-daVinci/portfolio@d9001119b75231544e87279d4045c0de5980408d 🚀

---
+++ _next/static/chunks/252f366e-b1fea287c933764f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,n,a){a.d(n,{K5g:function(){return h}});var e=a(8357);function h(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0V0z"}},{tag:"path",attr:{d:"M21 11.01L3 11v2h18zM3 16h12v2H3zM21 6H3v2.01L21 8z"}}]})(t)}}}]);
